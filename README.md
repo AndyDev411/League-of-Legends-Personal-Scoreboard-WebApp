78907ccf4fa9b600e44fe1c5aeaa946670f8e2f8
AndyHud411 <andygamedev411@gmail.com>
2022-11-26 01:34:19 -0500

    Version 1.0
    Project's Current Features :
    + Displaying Username
    + Displaying Summoner Level
    + Displaying Summoner icon
    + Displaying Summoner Solo Queue Rank (Only one that Matters!)
    + Displaying Player Score (Net League Points / Time Played)
    + Ranking Your Scoreboard
    
    Features Still To Be Added :
    - Removing People From Leader Board.
    - Displaying more stats when User Card is clicked (Maybe, I might just keep it Simple)

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# League of Legends Personal Scoreboard WebApp
- A Webapp that Displays Account data and scores them based on their League Points per Hour and stores the info locally on the broweser for general use.
+# League of Legends Personal Scoreboard Website
+
+LoL Personal Scoreboard Website was a project made becuase there was no direct 
+way to compare your skills side by side with your friends or other favorite players.
+So I developed this web app using React, and the Riot Games API. Lol Personal Scoreboard
+scores players based on their League Points gained per Hour, the displays them side by side
+for you to compare. Very Straight forward but I really just wanted a reason to use the Riot
+API besides making an op.gg clone...
+
+
+## Getting Started
+
+Instructions Coming Soon...
+
+### Prerequisites
+
+Requirements for the software and other tools to build, test and push 
+- [React](https://github.com/facebook/react)
+- [Riot API Key](https://developer.riotgames.com/)
+
+### Installing
+
+Installing Instructions Coming Soon...
+aaa
+## Running the tests
+
+Just run the App using 'npm start' in your console and it should bring you to the webpage.
+Then Just Type in a Username you'd like to add to your Leaderboard to see if it is working.
+It should pop up the player in the container below the navbar in a few ms.
+
+### Sample Tests
+
+Simple Functionality Test.
+
+### Style test
+
+Checks if the best practices and the right coding style has been used.
+
+    Give an example
+
+## Deployment
+
+Don't Recommend Deploying this. I just wanted to showcase my project which might be able to help other people out with the RIOT API...
+
+## Contributing
+
+Me and the Riot Games API...
+
+## Versioning
+
+It is Version 1.0
+
+## Authors
+
+  - **Andrew Hudson** -
+  (LINK TO WEBSITE COMING SOON...)
+
+
+## License
+
+- [Riot API Licensing](https://developer.riotgames.com/terms)
+
+## Acknowledgments
+
+  - RIot team for giving out their API for free... Favorite API by far... my Username is BIGSSMMOOKKEE if you wanna add me to your lol Personal Leaderboard (press X to doubt).
+
---
+++ src/App.js
@@ -0,0 +1,411 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import lolLogo from './league-of-legends.png';
+import './App.css';
+
+
+function App() {
+
+  // Constants
+  const MY_API_KEY = 'RGAPI-b2cf2007-bc70-41f6-88fe-112e8eba0f6d'; // My API Access Key
+
+  
+  
+
+  // Varitables
+  const [searchText, setSearchText] = useState(""); // searchText : string varitable that stores the text that is currently in the input box
+  const [playerData, setPlayerData] = useState({}); // playerData : a varitable that is often used for showing the players information in the html 
+  const [rankData, setRankData] = useState({});     // rankData   : a varitable that is often used for showing a players ranked information in the html 
+  const [isLoading, setLoading] = useState(true);   // isLoading  : a varitable that is tells the DOM that we are waiting for information (Deprecated...)
+  const [users, setUsers] = useState([]);           // users      : an array of user objects that holds the information of the player (ranked and summoner info)
+  const [started, setStarted] = useState(false);
+
+  onStart(); // Call start function so we can init the previously used list
+
+  // Author      : Andrew Hudson
+  // Name        : searchForPlayerr(event)
+  // Description : This function takes the input from the input box that is being live updated in the 'searchText' varitable!
+  function searchForPlayer(event)
+{
+    // Set Up correct API call
+    var APICallString = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + MY_API_KEY;
+    // Handle Api call
+     axios.get(APICallString).then(function(response)
+    {
+      console.log(response.data);
+      setPlayerData(response.data); // Saving player Data
+      var myPlayerData = response.data;
+      var APIRankedCallString = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + response.data.id + "?api_key=" + MY_API_KEY;
+      setLoading(true);
+      axios.get(APIRankedCallString).then(function(response)
+      {
+        if(JSON.stringify(response.data) == "[]")
+        {
+          var myRankedData = {};
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          console.log(userToAdd);
+          users.push(userToAdd);
+          localStorage.setItem(localStorage.length, myPlayerData.name);
+        }
+
+        else if(response.data[0].queueType == "RANKED_SOLO_5x5")
+        {
+            setRankData(response.data[0]);
+            var myRankedData = response.data[0];
+            var score = 0;
+            var userToAdd = {myPlayerData, myRankedData, score};
+            scoreUser(userToAdd);
+            console.log(userToAdd);
+            users.push(userToAdd);
+            localStorage.setItem(localStorage.length, myPlayerData.name);
+            
+        }
+        else if(response.data[0].queueType == "RANKED_FLEX_SR")
+        {
+          setRankData(response.data[1]);
+          var myRankedData = response.data[1];
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          console.log(userToAdd);
+          users.push(userToAdd);
+          localStorage.setItem(localStorage.length, myPlayerData.name);
+        }
+        else
+        {
+          var myRankedData = {};
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          console.log(userToAdd);
+          users.push(userToAdd);
+          localStorage.setItem(localStorage.length, myPlayerData.name);
+        }
+  
+        setLoading(false);
+          
+
+        }).catch(function (error){
+          var myRankedData = {};
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          console.log(userToAdd);
+          users.push(userToAdd);
+          console.log(error);
+          localStorage.setItem(localStorage.length, myPlayerData.name);
+        });
+
+        sortUsers();
+        console.log();
+
+      // Success
+    }).catch(function (error){
+
+
+      setPlayerData({}); 
+      setRankData({});
+      // Error
+    });
+    // Get Ranked Info 
+}
+
+  // Author      : Andrew Hudson
+  // Name        : addPlayer(playerName)
+  // Description : This function takes in a Leauge of Legends username (playerName) and then adds it to the array of users (users)
+ async  function addPlayerByName(playerName)
+  {
+    if(playerName == "null")
+    {return;}
+    // Set Up correct API call
+    var APICallString = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + String(playerName) + "?api_key=" + MY_API_KEY;
+    // Handle Api call
+     axios.get(APICallString).then(function(response)
+    {
+      setPlayerData(response.data); // Saving player Data
+      var myPlayerData = response.data;
+      var APIRankedCallString = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + response.data.id + "?api_key=" + MY_API_KEY;
+      setLoading(true);
+      axios.get(APIRankedCallString).then(function(response)
+      {
+        if(JSON.stringify(response.data) == "[]")
+        {
+          var myRankedData = {};
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          users.push(userToAdd);
+        }
+
+        else if(response.data[0].queueType == "RANKED_SOLO_5x5")
+        {
+            setRankData(response.data[0]);
+            var myRankedData = response.data[0];
+            var score = 0;
+            var userToAdd = {myPlayerData, myRankedData, score};
+            scoreUser(userToAdd);
+            users.push(userToAdd);
+            
+        }
+        else if(response.data[0].queueType == "RANKED_FLEX_SR")
+        {
+          setRankData(response.data[1]);
+          var myRankedData = response.data[1];
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          users.push(userToAdd);
+        }
+        else
+        {
+          var myRankedData = {};
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          users.push(userToAdd);
+          
+        }
+  
+          
+
+        }).catch(function (error){
+          var myRankedData = {};
+          var score = 0;
+          var userToAdd = {myPlayerData, myRankedData, score};
+          scoreUser(userToAdd);
+          users.push(userToAdd);
+          console.log(error);
+        });
+
+
+
+
+      // Success
+    }).catch(function (error){
+
+
+      setPlayerData({}); 
+      setRankData({});
+      // Error
+    });
+    // Get Ranked Info 
+  }
+
+  // Author      : Andrew Hudson
+  // Name        : GiveUser(user, userRank)
+  // Description : this function draws the summoner cards using the user object (user) and ranked information from the userRank (userRank) (DEPRECATED...)
+  function GiveUser(user, userRank)
+  {
+    return(
+    <div className='user'>
+    <p>{user.name}</p>
+    <img width={100} height={100} src={"http://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/" + user.profileIconId + ".png"}></img>
+
+  </div>)
+  }
+
+  // Author      : Andrew Hudson
+  // Name        : printUsers()
+  // Description : This function prints all the users
+  function printUsers()
+  {
+
+    return(
+
+        GiveUser(playerData, rankData)
+    )
+    
+
+  }
+  
+  // Author      : Andrew Hudson
+  // Name        : renderUsers(user, index)
+  // Description : This function is used in the array.mapping() function callback paramater and prints all the objects in the array of users as the summoner cards
+  const renderUser = (user, index) => {
+
+    return(
+    <li key = {index}>
+      <div className='user'>
+        <h1> # {index + 1}</h1>
+        <h1>{user.myPlayerData.name}</h1>
+        <img width={200} height={200} src={"http://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/" + user.myPlayerData.profileIconId + ".png"}></img>
+        <h2>Summoner Level {user.myPlayerData.summonerLevel}</h2>
+        {JSON.stringify(user.myRankedData) != '{}' ? <h3>Summoner Solo Queue Rank : {user.myRankedData.tier} {user.myRankedData.rank}</h3> : <h3>Summoner is Unranked...</h3>}
+        <h2>{Math.round(user.score)} LP/HR</h2>
+      </div>
+    </li>
+    )
+  }
+
+  // Author      : Andrew Hudson
+  // Name        : sortUsers()
+  // Description : This function sorts all the users in the array by their overall score so it will be printed in order
+  function sortUsers()
+  {
+    for(var i = 0; i <= users.length - 1; i++)
+    {
+      for(var j = 0; j < ( users.length - i -1); j++)
+      {
+        if(users[j].score < users[j+1].score)
+        {
+            // swap em
+            var tempUser = users[j];
+            users[j] = users[j+1];
+            users[j+1] = tempUser;
+        }
+      }
+    }
+
+  }
+
+  // Author      : Andrew Hudson
+  // Name        : scoreUser(userToScore)
+  // Description : This function scores the given user (userToScore) based on the amount of LP they've gotten / total time played
+  function scoreUser(userToScore)
+  {
+
+    if(JSON.stringify(userToScore.myRankedData) == "{}")
+    {
+      score = 0;
+      return;
+    }
+
+      // Get users data
+      var timePlayed = ((userToScore.myRankedData.wins + userToScore.myRankedData.losses) * 35) / 60; // Get the hours played
+      var lpAchieved = (getRankDivisionNumber(userToScore.myRankedData)) + userToScore.myRankedData.leaguePoints;
+      var score = lpAchieved / timePlayed;
+      // Calculate score
+
+      
+
+      // SetScore
+      userToScore.score = score;
+
+      //console.log(userToScore);
+  }
+
+  // Author      : Andrew Hudson
+  // Name        : getRankedDivisionNumber(rankData)
+  // Description : This function returns the amount of lp the given user has (rankedData)
+  function getRankDivisionNumber(rankedData)
+  {
+
+    var myTier = rankedData.tier;
+    var myRank = rankedData.rank;
+    var myBaseScore;
+    switch(String(myRank))
+    {
+       case "IV":
+          // code block
+          myBaseScore = 4;
+          break;
+        case "III":
+          // code block
+          myBaseScore = 3;
+          break;
+        case "II":
+          // code block
+          myBaseScore = 2;
+          break;
+        case "I":
+          myBaseScore = 1;
+          break;
+        default :
+          myBaseScore = 100;
+          break;
+          // code block
+    }
+    
+    switch(String(myTier)) {
+        case "IRON":
+          // code block
+          myBaseScore = (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "BRONZE":
+          // code block
+          myBaseScore = (4 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "SILVER":
+          // code block
+          myBaseScore = (8 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "GOLD":
+          // code block
+          myBaseScore = (12 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "PLATINUM":
+          // code block
+          myBaseScore = (16 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "DIAMOND":
+          // code block
+          myBaseScore = (20 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "MASTER":
+          // code block
+          myBaseScore = (24 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "GRANDMASTER":
+          // code block
+          myBaseScore = (28 * 100) + (myBaseScore - 1) * 100 + rankedData.leaguePoints;
+          break;
+        case "CHALLENGER":
+          // code block
+          myBaseScore = (32 * 100) + rankedData.leaguePoints;
+          break;
+        default:
+        myBaseScore = 0;
+          // code block
+      } 
+      return myBaseScore;
+  }
+
+  // Author      : Andrew Hudson
+  // Name        : onStart() (async)
+  // Description : This function runs only once when the website is first opened!
+  async function onStart()
+  {
+
+    //localStorage.clear();
+    
+    if(localStorage.length > 0 && started == false)
+    {
+      console.log("start started...");
+      for(var  i = 0; i < localStorage.length; i++)
+      {
+        var playerName = localStorage.getItem(i);
+        console.log(playerName);
+        await addPlayerByName(String(playerName));
+      }
+      sortUsers();
+      await setStarted(true);
+    }
+  }
+
+  // Final return statement!
+  return (
+    <div className="App">
+      <div className='container'>
+        <h5> <img src={lolLogo} width={125} height={125}></img> League of Legends : Personal Leaderboard</h5>
+        <input typeof='text' onChange={e => setSearchText(e.target.value)}></input>
+        <button onClick={e => searchForPlayer(e)}>ADD PLAYER</button>
+      </div>
+      {JSON.stringify(users) != '[]'
+      ? 
+      <>
+          {sortUsers()}
+          {users.map(renderUser)}
+      </>
+        
+
+      : <><div className='noUser'>NO SUMMONER DATA</div></> // False
+      }
+
+    </div>
+  );
+}
+
+export default App;
